9c11fcb372ea23f2a8c1d505235623ea1f490d8f
sujanaray <raysujana@gmail.com>
2021-05-10 11:55:10 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Parcel Micro App!</h1>
+      <p>Hard to get more minimal than this React app. Hello!! I am Sujana</p>
+    </>
+  );
+};
+export default App;
